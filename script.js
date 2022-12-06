1d05d7d98e3ad1b1a170f3e96017b8fe953c65bb
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2022-12-06 00:50:33 +0000

    Style about me section

--- script.js
+++
@@ -1,9 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
-
-var typed = new Typed(".animate", {
-    strings: ["coder.", "student.", "aspiring web developer.", "football fan."],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-});
-  
